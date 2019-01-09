8de57d1ea17ec1dd190df9d174cd1f7b62da13da
Kyle <polysplitter@gmail.com>
2019-01-08 20:44:16 -0600

    played around with delete many

--- app.js
+++ app.js
@@ -4,29 +4,6 @@ const mongoose = require('mongoose')
 
 mongoose.connect('mongodb://localhost:27017/fruitsDB', { useNewUrlParser: true })
 
-const fruitSchema = new mongoose.Schema ({
-  name: {
-    type: String,
-    required: [ true, 'Names are required.' ] 
-  },
-  rating: {
-    type: Number,
-    min: 1,
-    max: 10,
-  },
-  review: String
-})
-
-const Fruit = mongoose.model('Fruit', fruitSchema)
-
-const fruit = new Fruit({
-  name: "Apple",
-  rating: 34,
-  review: "Pretty solid as a fruit"
-})
-
-//fruit.save()
-
 const personSchema = new mongoose.Schema ({
   name: String,
   age: Number
@@ -39,7 +16,51 @@ const person = new Person({
   age: 37
 })
 
-//person.save()
+// person.save()
+
+
+// Person.find((err, people) => {
+//   if (err) {
+//     console.log(err)
+//   } else {
+//     mongoose.connection.close()
+//     people.forEach(person => {
+//       console.log(person.name)
+//     })
+//   }
+// })
+
+Person.deleteMany({ name: 'Jim' }, (err) => {
+  if(err) {
+    console.log(err)
+  } else {
+    mongoose.connection.close()
+    console.log('success')
+  }
+})
+
+// const fruitSchema = new mongoose.Schema ({
+//   name: {
+//     type: String,
+//     required: [ true, 'Names are required.' ] 
+//   },
+//   rating: {
+//     type: Number,
+//     min: 1,
+//     max: 10,
+//   },
+//   review: String
+// })
+
+// const Fruit = mongoose.model('Fruit', fruitSchema)
+
+// const fruit = new Fruit({
+//   name: "Apple",
+//   rating: 34,
+//   review: "Pretty solid as a fruit"
+// })
+
+//fruit.save()
 
 // const kiwi = new Fruit({
 //   name: "Kiwi",
@@ -67,16 +88,16 @@ const person = new Person({
 //     }
 // })
 
-Fruit.find((err, fruits) => {
-  if (err) {
-    console.log(err)
-  } else {
-    mongoose.connection.close()
-    fruits.forEach(fruit => {
-      console.log(fruit.name)
-    })
-  }
-})
+// Fruit.find((err, fruits) => {
+//   if (err) {
+//     console.log(err)
+//   } else {
+//     mongoose.connection.close()
+//     fruits.forEach(fruit => {
+//       console.log(fruit.name)
+//     })
+//   }
+// })
 
 // Fruit.updateOne({ _id: '5c33e93b9c90130a710b0b81' }, { name: 'NewBanana' }, (err) => {
 //   if (err) {
